f41988dfc012099b57cb99d5b0c51d8e49c4402e
Shadowz3n <emp.shad@gmail.com>
2017-06-20 19:57:22 -0300

    Update

--- www/js/script.js
+++ www/js/script.js
@@ -2,8 +2,30 @@
 $(document).ready(function(){
 	
 	/* App login */
+	/* App login */
 	$(document).on("submit", ".login_form", function(){
-		
+		var thisForm	= $(this);
+		$.ajax({
+			url:document.location.href,
+			type:"POST",
+			data:$(thisForm).serialize(),
+			beforeSend:function(){
+				$(thisForm).find("input, button").prop({disabled:true});
+				$(".btn_login_form").find("div").removeClass("uk-hidden").end().find("span").html(" CARREGANDO..");
+			}
+		}).done(function(data){
+			if(Number(data)>0){
+				//document.location.reload();
+			}else{
+				UIkit.notification('Login ou senha incorretos, por favor tente novamente.', 'danger');
+				$(thisForm).find("input, button").prop({disabled:false});
+				$(".btn_login_form").find("div").addClass("uk-hidden").end().find("span").html(" ENTRAR");
+			}
+		}).fail(function(){
+			UIkit.notification('Ocorreu um erro ao fazer o login, por favor tente novamente.', 'danger');
+			$(thisForm).find("input, button").prop({disabled:false});
+			$(".btn_login_form").find("div").addClass("uk-hidden").end().find("span").html(" ENTRAR");
+		});
 		return false;
 	});
 });
