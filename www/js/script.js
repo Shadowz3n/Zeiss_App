4e2c4bef32a18d5ec090d3deda0664f357b0f6fd
Shadowz3n <emp.shad@gmail.com>
2017-06-21 15:52:55 -0300

    Update

--- www/js/script.js
+++ www/js/script.js
@@ -43,9 +43,11 @@ $(document).ready(function(){
 		
 		lucro_mensal	= ((zeiss_venda-zeiss_custo) - (sugestao_venda-sugestao_custo));
 		lucro_anual		= lucro_mensal*12;
-			
+		lucro_sao_web	= lucro_anual*0.02;
+		
 		$("#economia_mensal").html(lucro_mensal.toLocaleString('pt-BR', { minimumFractionDigits:2 , currency:'BRL' }));
 		$("#economia_anual").html(lucro_anual.toLocaleString('pt-BR', { minimumFractionDigits:2 , currency:'BRL' }));
+		$("#economia_sao_web").html(lucro_sao_web.toLocaleString('pt-BR', { minimumFractionDigits:2 , currency:'BRL' }));
 	}
 	
 	$.calc();
@@ -65,6 +67,7 @@ $(document).ready(function(){
 		}).done(function(data){
 			console.log(data);
 			if(Number(data)>0){
+				window.localStorage.user_id	= Number(data);
 				$(".zeiss_login").stop().fadeOut(250, function(){
 					$(".app").stop().fadeIn(250);
 					$(".login_form").find("input, button").prop({disabled:false});
@@ -74,6 +77,7 @@ $(document).ready(function(){
 				UIkit.notification('Login ou senha incorretos, por favor tente novamente.', 'danger');
 				$(thisForm).find("input, button").prop({disabled:false});
 				$(".btn_login_form").find("div").addClass("uk-hidden").end().find("span").html(" ENTRAR");
+				window.localStorage.removeItem("user_id");
 			}
 		}).fail(function(){
 			UIkit.notification('Ocorreu um erro ao fazer o login, por favor tente novamente.', 'danger');
@@ -81,12 +85,26 @@ $(document).ready(function(){
 			$(".btn_login_form").find("div").addClass("uk-hidden").end().find("span").html(" ENTRAR");
 		});
 		return false;
+	}).on("submit", ".add_comparation_form", function(){
+		$(".table_sugestao").find("tbody").append('');
+		UIkit.modal("#add_comparation_modal").hide();
+		$("#add_comparation_modal").find("input").val("");
+		console.log(123);
+		return false;
 	}).on("click", ".exit_app", function(){
 		$(".app").stop().fadeOut(250, function(){
 			$(".zeiss_login").stop().fadeIn(250);
 		});
 		$("#offcanvas-nav").click();
+		window.localStorage.removeItem("user_id");
 		return false;
 	});
+	
+	if(window.localStorage.user_id){
+		$(".zeiss_login").css({display:'none'});
+		$(".app").css({display:'block'});
+		$(".login_form").find("input, button").prop({disabled:false});
+		$(".btn_login_form").find("div").addClass("uk-hidden").end().find("span").html(" ENTRAR");
+	}
 });
 //}, false);
